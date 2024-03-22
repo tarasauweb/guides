ccfdc5dd3f46ba668f0689f7d560ef3f473d61d5
Artur Tarasau <tarasov.a.web@gmail.com>
2024-03-22 15:26:23 +0300

    feat:about-server-page

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-import './styles/styles.scss'
+import './styles/styles.scss';
+
+const content = document.querySelector('.content') as HTMLElement;
+
+function include(address:string){
+  fetch(address).then(resp => resp.text())
+  .then(data => { content.innerHTML = data } )
+}
+
+include('./about-server-page.html');
